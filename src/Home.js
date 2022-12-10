78b1cfce8d94b94dfd7ddd67a5f3b4a284dbcbb2
João Gabriel Lembo <joaogabriellembo@usp.br>
2022-12-10 02:22:17 -0300

    Adicione upload de arquivos

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,24 @@
+import React, {createRef} from "react";
 import {postSimulate} from "./API.js";
+import FileUpload from "./FileUpload.js";
 
 function Home() {
+
+  const fileUploadRef = createRef();
+
+  function activateSimulation() {
+    console.log(fileUploadRef);
+    let result = postSimulate();
+    console.log(result);
+  }
+
   return (
     <div>
       <h1>CIRCULARES - SIMULADOR DE DEMANDA</h1>
+      <FileUpload ref={fileUploadRef}/>
       <button onClick = {activateSimulation}>Simular Resultados</button>
     </div>
   );
-}
-
-function activateSimulation() {
-  let result = postSimulate();
-  console.log(result);
 }
 
 export default Home;
