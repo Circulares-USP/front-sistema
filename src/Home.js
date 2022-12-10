06348bd6d8de1e1b633ab2a006e4fccebecc06dc
José Lucas Silva Mayer <josemayer@usp.br>
2022-12-10 03:05:24 -0300

    Adiciona botão de mostrar os resultados obtidos da API
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,26 @@
-import {postSimulate} from "./API.js";
+import {postSimulate, getSimulate} from "./API.js";
+import {useState, useEffect} from "react";
 
 function Home() {
+  const [isShown, setIsShown] = useState(false);
+  
+  const showSimulateResults = event => {
+    setIsShown(current => true);
+    let result = getSimulate();
+    result.then((data) => {
+	// show data on the page;
+    	console.log(data);
+    });
+  }
+ 
   return (
     <div>
       <h1>CIRCULARES - SIMULADOR DE DEMANDA</h1>
       <button onClick = {activateSimulation}>Simular Resultados</button>
+      <button onClick = {showSimulateResults}>Mostrar resultados</button>
+      {isShown && (
+	      <ShowResult/>
+      )}
     </div>
   );
 }
@@ -13,5 +29,32 @@ function activateSimulation() {
   let result = postSimulate();
   console.log(result);
 }
+
+function ShowResult() {
+  const [apiResponse, setApiResponse] = useState("");
+
+  useEffect(() => {
+      getSimulate().then(
+          result => setApiResponse(result));
+  },[]);
+
+  return(
+      <div>
+          <h1>Resultados</h1>
+          <div>
+	  {
+	    Object.keys(apiResponse)
+		.map( (obj, i) => (
+			<div>
+			  <h3 key="{i}">{obj}</h3>
+		  	  <p>{JSON.stringify(apiResponse[obj])}</p>
+			</div>
+		  )	
+		)
+	 }
+         </div>
+      </div>
+  );
+};
 
 export default Home;
