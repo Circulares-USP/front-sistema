614d70238e3dd0ec77e1ce05ca6f3305f3134a5e
José Lucas Silva Mayer <josemayer@usp.br>
2022-12-10 03:00:48 -0300

    Exporta função do método GET
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/API.js
+++ src/API.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const url = process.env.REACT_APP_BACKEND_URL;
 
-// const fetchApi = (base, params = {}) => {
-//   const request = url + base;
+const fetchApi = (base, params = {}) => {
+   const request = url + base;
 
-//   return axios.get(request, { params }).then((res) => res.data);
-// };
+   return axios.get(request, { params }).then((res) => res.data);
+};
 
 const postApi = (base, params = {}) => {
   const request = url + base;
@@ -26,6 +26,10 @@ const postApi = (base, params = {}) => {
 
 const postSimulate = () => { return ({"batata": 1})};
 
+const getSimulate = () =>
+	fetchApi("").then((data) => data);
+
 export {
     postSimulate,
+    getSimulate
 };
