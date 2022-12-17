e34b39bb0a5222da5538a117e4c218eacca8d5a5
João Gabriel Lembo <joaogabriellembo@usp.br>
2022-12-17 17:16:40 -0300

    Altera formato do json da demanda

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -24,32 +24,91 @@ class FileUpload extends React.Component{
 		xlsxToJson(event.target.files[0], this.formatDepartureJson);
 	};
 
-	formatDemandJson = (rawDemand) => {
+	formatDemandJson = (demandaIdaButanta, demandaIdaP3, demandaVoltaButanta, demandaVoltaP3) => {
 		var parsedDemand = {
-			480: {
-				"seg": {},
-				"ter": {},
-				"qua": {},
-				"qui": {},
-				"sex": {}
+			"ida_manha": {
+				"de_p3": {
+					"seg": {},
+					"ter": {},
+					"qua": {},
+					"qui": {},
+					"sex": {},
+				},
+				"de_butanta": {
+					"seg": {},
+					"ter": {},
+					"qua": {},
+					"qui": {},
+					"sex": {},
+				}
 			},
-			1140: {
-				"seg": {},
-				"ter": {},
-				"qua": {},
-				"qui": {},
-				"sex": {}
+			"ida_tarde": {
+				"de_p3": {
+					"seg": {},
+					"ter": {},
+					"qua": {},
+					"qui": {},
+					"sex": {},
+				},
+				"de_butanta": {
+					"seg": {},
+					"ter": {},
+					"qua": {},
+					"qui": {},
+					"sex": {},
+				}
+			},
+			"volta_tarde": {
+				"de_p3": {
+					"seg": {},
+					"ter": {},
+					"qua": {},
+					"qui": {},
+					"sex": {},
+				},
+				"de_butanta": {
+					"seg": {},
+					"ter": {},
+					"qua": {},
+					"qui": {},
+					"sex": {},
+				}
 			}
 		}
 		for (let i = 0; i < 10; i++) {
-			let hour = rawDemand[i].Hora == "08:00" ? 480 : 1140;
-			let day = convertDayNames(rawDemand[i].Dia);
+			let hourButanta = demandaIdaButanta[i].Hora === "08:00" ? "ida_manha" : "ida_tarde";
+			let dayButanta = convertDayNames(demandaIdaButanta[i].Dia);
 
-			for (const [stop, value] of Object.entries(rawDemand[i])) {
-				if (stop == "Hora" || stop == "Dia") continue;
-				parsedDemand[hour][day][stop] = value;
+			for (const [stop, value] of Object.entries(demandaIdaButanta[i])) {
+				if (stop === "Hora" || stop === "Dia") continue;
+				parsedDemand[hourButanta]["de_butanta"][dayButanta][stop] = value;
+			}
+
+			let hourP3 = demandaIdaP3[i].Hora === "08:00" ? "ida_manha" : "ida_tarde";
+			let dayP3 = convertDayNames(demandaIdaP3[i].Dia);
+
+			for (const [stop, value] of Object.entries(demandaIdaP3[i])) {
+				if (stop === "Hora" || stop === "Dia") continue;
+				parsedDemand[hourP3]["de_p3"][dayP3][stop] = value;
 			}
 		}
+
+		for (let i = 0; i < 5; i++) {
+			let dayButanta = convertDayNames(demandaVoltaButanta[i].Dia);
+
+			for (const [stop, value] of Object.entries(demandaVoltaButanta[i])) {
+				if (stop === "Hora" || stop === "Dia") continue;
+				parsedDemand["volta_tarde"]["de_butanta"][dayButanta][stop] = value;
+			}
+
+			let dayP3 = convertDayNames(demandaVoltaP3[i].Dia);
+
+			for (const [stop, value] of Object.entries(demandaVoltaP3[i])) {
+				if (stop === "Hora" || stop === "Dia") continue;
+				parsedDemand["volta_tarde"]["de_p3"][dayP3][stop] = value;
+			}
+		}
+		
 		this.setState({demandJson: parsedDemand});
 	}
 
@@ -84,10 +143,24 @@ function xlsxToJson(file, callback) {
 	reader.onload = (evt) => { 
 		const bstr = evt.target.result;
 		const wb = XLSX.read(bstr, {type:'binary'});
-		const wsname = wb.SheetNames[0];
-		const sheet = wb.Sheets[wsname];
-		const data = XLSX.utils.sheet_to_json(sheet);
-		callback(data);
+
+		const wsnameIdaButanta = wb.SheetNames[0];
+		const sheetIdaButanta = wb.Sheets[wsnameIdaButanta];
+		const dataIdaButanta = XLSX.utils.sheet_to_json(sheetIdaButanta);
+
+		const wsnameIdaP3 = wb.SheetNames[0];
+		const sheetIdaP3 = wb.Sheets[wsnameIdaP3];
+		const dataIdaP3 = XLSX.utils.sheet_to_json(sheetIdaP3);
+		
+		const wsnameVoltaButanta = wb.SheetNames[0];
+		const sheetVoltaButanta = wb.Sheets[wsnameVoltaButanta];
+		const dataVoltaButanta = XLSX.utils.sheet_to_json(sheetVoltaButanta);
+		
+		const wsnameVoltaP3 = wb.SheetNames[0];
+		const sheetVoltaP3 = wb.Sheets[wsnameVoltaP3];
+		const dataVoltaP3 = XLSX.utils.sheet_to_json(sheetVoltaP3);
+
+		callback(dataIdaButanta, dataIdaP3, dataVoltaButanta, dataVoltaP3);
 	};
 	reader.readAsBinaryString(file);
 }
