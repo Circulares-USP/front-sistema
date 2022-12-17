b7b7a44c87061a6e63116a40379f317b1dc1309c
João Gabriel Lembo <joaogabriellembo@usp.br>
2022-12-16 23:51:20 -0300

    Adiciona parse dos arquivos no front

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as XLSX from 'xlsx';
 
 class FileUpload extends React.Component{
 
@@ -8,19 +9,64 @@ class FileUpload extends React.Component{
 			{selectedDemandFile: null,
 			isDemandFileSelected: false,
 			selectedDepartureFile: null,
-			isDepartureFileSelected: false}
+			isDepartureFileSelected: false,
+			demandJson: {},
+			departureJson: {}}
 	}
 
 	changeDemandFile = (event) => {
 		this.setState({selectedDemandFile: event.target.files[0], isDemandFileSelected: true});
+		xlsxToJson(event.target.files[0], this.formatDemandJson);
 	};
 
 	changeDepartureFile = (event) => {
 		this.setState({selectedDepartureFile: event.target.files[0], isDepartureFileSelected: true});
+		xlsxToJson(event.target.files[0], this.formatDepartureJson);
 	};
 
-	handleSubmission = () => {
-	};
+	formatDemandJson = (rawDemand) => {
+		var parsedDemand = {
+			480: {
+				"seg": {},
+				"ter": {},
+				"qua": {},
+				"qui": {},
+				"sex": {}
+			},
+			1140: {
+				"seg": {},
+				"ter": {},
+				"qua": {},
+				"qui": {},
+				"sex": {}
+			}
+		}
+		for (let i = 0; i < 10; i++) {
+			let hour = rawDemand[i].Hora == "08:00" ? 480 : 1140;
+			let day = convertDayNames(rawDemand[i].Dia);
+
+			for (const [stop, value] of Object.entries(rawDemand[i])) {
+				if (stop == "Hora" || stop == "Dia") continue;
+				parsedDemand[hour][day][stop] = value;
+			}
+		}
+		this.setState({demandJson: parsedDemand});
+	}
+
+	formatDepartureJson = (rawDeparture) => {
+		var parsedDeparture = {
+			8012: [],
+			8022: [],
+			8032: []
+		}
+		for (let i = 0; i < 24; i++) {
+			for (let j = 8012; j < 8042; j += 10) {
+				parsedDeparture[j].push(rawDeparture[i][j])
+			}
+		}
+		console.log(parsedDeparture);
+		this.setState({departureJson: parsedDeparture});
+	}
 
 	render() {
 		return(
@@ -31,6 +77,23 @@ class FileUpload extends React.Component{
 			</div>
 		)
 	}
+}
+
+function xlsxToJson(file, callback) {
+	const reader = new FileReader();
+	reader.onload = (evt) => { 
+		const bstr = evt.target.result;
+		const wb = XLSX.read(bstr, {type:'binary'});
+		const wsname = wb.SheetNames[0];
+		const sheet = wb.Sheets[wsname];
+		const data = XLSX.utils.sheet_to_json(sheet);
+		callback(data);
+	};
+	reader.readAsBinaryString(file);
+}
+
+function convertDayNames(day) {
+	return day.toLowerCase().substring(0, 3);
 }
 
 export default FileUpload;
