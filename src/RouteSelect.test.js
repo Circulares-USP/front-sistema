adc3f4d4d75a55eb6ec99edc7c35de6de82ae264
José Lucas Silva Mayer <josemayer@usp.br>
2022-12-18 13:29:54 -0400

    Adiciona testes de renderização de componentes principais
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

---
+++ src/RouteSelect.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import RouteSelect from './RouteSelect';
+import BusStops from './BusStops';
+
+beforeEach(() => {
+  const select8012 = render(<RouteSelect line={8012} />);
+  const select8022 = render(<RouteSelect line={8022} />);
+  const select8032 = render(<RouteSelect line={8032} />);
+});
+
+it('renderiza corretamente o título de seleção de pontos', () => {
+  const select_title1 = screen.getByText(/Selecione a rota da linha 8012/i);
+  const select_title2 = screen.getByText(/Selecione a rota da linha 8022/i);
+  const select_title3 = screen.getByText(/Selecione a rota da linha 8032/i);
+  expect(select_title1).toBeInTheDocument();
+  expect(select_title2).toBeInTheDocument();
+  expect(select_title3).toBeInTheDocument();
+});
+
+it('renderiza corretamente todas as paradas de ônibus', () => {
+  for (let i = 0; i < BusStops.length; i++) {
+  	const select_option = screen.getAllByText(BusStops[i])[0];
+	expect(select_option).toBeInTheDocument();
+  }
+});
--- src/components/ResultsPoints.test.js
+++ src/components/ResultsPoints.test.js
@@ -1,15 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import ResultsPoints from './ResultsPoints';
 
+beforeEach(() => {
+  const resultsPoints = render(<ResultsPoints />);
+});
 
-test('renderiza a seção de resultados corretamente', () => {
-  render(<ResultsPoints />);
+it('renderiza a seção de resultados corretamente', () => {
   const resultElement = screen.getByText(/Resultados/i);
   expect(resultElement).toBeInTheDocument();
 });
 
-test('renderiza o título da média por ponto', () => {
-  render(<ResultsPoints />);
+it('renderiza o título da média por ponto', () => {
   const meanByPointElement = screen.getByText(/Média por ponto/i);
   expect(meanByPointElement).toBeInTheDocument();
 });
